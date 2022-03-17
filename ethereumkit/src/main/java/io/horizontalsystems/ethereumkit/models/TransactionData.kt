4e00d0a1547f7388b633d85c548841b265ba2d58
Dimas Victor <quetzalcroix@gmail.com>
2022-03-17 23:28:45 +0700

    Adding Transaction Data

--- ethereumkit/src/main/java/io/horizontalsystems/ethereumkit/models/TransactionData.kt
+++ ethereumkit/src/main/java/io/horizontalsystems/ethereumkit/models/TransactionData.kt
@@ -5,16 +5,18 @@ import java.math.BigInteger
 import java.util.*
 
 data class TransactionData(
-        val from: Address? = null,
-        val to: Address,
-        val value: BigInteger,
-        val input: ByteArray,
-        val nonce: Long? = null
+    val from: Address? = null,
+    val to: Address,
+    val value: BigInteger,
+    val input: ByteArray,
+    val nonce: Long? = null
 ) {
     override fun equals(other: Any?): Boolean {
         return when {
             this === other -> true
-            other is TransactionData -> to == other.to && value == other.value && input.contentEquals(other.input) && nonce == other.nonce
+            other is TransactionData -> to == other.to && from == other.from && value == other.value && input.contentEquals(
+                other.input
+            ) && nonce == other.nonce
             else -> false
         }
     }
